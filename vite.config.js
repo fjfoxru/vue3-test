c90cd68c3a18e5907ad02fb55074e7ac703f50a9
AlexL <as.lyashenko@ined.ru>
2023-11-29 16:42:05 +0300

    vite-config-fix

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,23 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          var info = assetInfo.name.split(".");
+          var extType = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          } else if (/woff|woff2/.test(extType)) {
+            extType = "css";
+          }
+          return `/vue3-test/static/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "/vue3-test/static/js/[name]-[hash].js",
+        entryFileNames: "/vue3-test/static/js/[name]-[hash].js",
+      },
+    }
+  },
 })
