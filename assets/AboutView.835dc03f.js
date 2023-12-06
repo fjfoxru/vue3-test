22cf98cdaa84ef62fab041ab836d91aec19c223c
fjfoxru <fjfox@yandex.ru>
2023-12-06 09:15:35 +0000

    ac1527e

---
+++ assets/AboutView.835dc03f.js
@@ -0,0 +1,1 @@
+import{v as t}from"./vAutofocus.3eb1d22e.js";import{b as s,d as o,f as a,u as r,e}from"./index.9c93e743.js";const c={class:"about"},n=e("h1",null,"\u041E \u043F\u0440\u0438\u043B\u043E\u0436\u0435\u043D\u0438\u0438",-1),m={__name:"AboutView",setup(u){return(_,l)=>(s(),o("div",c,[n,a(e("textarea",null,null,512),[[r(t)]])]))}};export{m as default};
